5eb1b6d59fb01a5e5f70cafe8606ea70bf8029ba
bala-amypo <bala@amypo.in>
2025-12-17 06:05:54 +0000

    User commit

--- src/main/java/com/example/demo/entity/Vehicle.java
+++ src/main/java/com/example/demo/entity/Vehicle.java
@@ -5,5 +5,17 @@ import jarkarta.persistence.*;
 public class Vehical{
 
     @id
-    @
+    @GenerateValue(strategy=GenerationType.IDENTITY)
+    Private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @Column(unique=true,nullable=false)
+    private String vehicalNumber;
+
+    private Double fuelEfficiency;
+
+    public Vehicle(){}
+    }
 }

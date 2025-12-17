cf5f86846dbec2141857f83c06dbb5e511162736
bala-amypo <bala@amypo.in>
2025-12-17 06:25:54 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Location.java
@@ -0,0 +1,24 @@
+
+package com.example.demo.entity;
+
+import jarkarta.persistence.*;
+
+@Entity
+public class Location{
+
+    @Id
+    @GenerateValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double latitude;
+    private Double longitude;
+
+    public Location(){}
+
+    public Long getId(){
+        return id;
+    }
+
+    public Long 
+
+}
